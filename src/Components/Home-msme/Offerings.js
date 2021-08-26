555fdeb1f7bde81b4e8cc0cd01284bf3d65352c7
varshitha1707 <varshithasreddy@gmail.com>
2021-08-26 10:53:17 +0530

    .

--- src/Components/Home-msme/Offerings.js
+++ src/Components/Home-msme/Offerings.js
@@ -64,12 +64,15 @@ export default function Offerings() {
         justifyContent="center"
         alignItems="center"
         >
+            {/* mapping out the cards information */}
             {offering.map(note =>(
             <Grid item xs={12} sm={8} md={6} lg={4}>
+                    {/* Card image */}
                 <Card id="Card" elevation={2}>
                     <CardMedia align="center" id="Card-img">
                         <img src={note.img} alt="img" />
                     </CardMedia>
+                    {/* Card title and description */}
                     <CardContent>
                         <Typography 
                         id="subtitle"
